ff9b5e82970f770044c4bd5ee7b8b85bb4a92164
diego galviz <dagalviz83@soy.sena.edu.co>
2023-09-29 20:03:18 -0500

    Feat/List tasks page done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,26 @@
+import TaskView from "@/components/home/TaskView";
 
-function HomePage() {
+async function loadTasks() {
+  const res = await fetch("http://localhost:3000/api/tasks");
+  const data = await res.json();
+  return data;
+}
 
-  return <div>Tareas</div>;
+async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          
+            <TaskView task={task} key={task.id} />
+          
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
